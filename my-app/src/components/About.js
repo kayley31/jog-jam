b7b83a658447d3e3788c796330ecc5cbe311f465
Jasmine Taylor <jazz.t1996@hotmail.com>
2024-05-14 20:30:05 +0100

    add react app with About.js

---
+++ my-app/src/components/About.js
@@ -0,0 +1,16 @@
+import './About.css'
+
+function About () {
+    return (
+        <>
+         <h1>Group 4 - About Us</h1>
+         <h2>Jasmine</h2>
+         <ul class='aboutul'>
+            <li>My favourite hobby is running</li>
+            <li>I do this because I enjoy the mental and physical challenge</li>
+         </ul>
+        </>     
+    )
+}
+
+export default About
