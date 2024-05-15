fe0d7975106d1fdfa61619075952fbb3c5fa6721
itskharina <137315976+itskharina@users.noreply.github.com>
2024-05-15 14:34:30 +0100

    Added my (Kharina's) section and wrapped sections in a div so that they all have aligned starting positions

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -4,11 +4,18 @@ function About () {
     return (
         <>
          <h1>Group 4 - About Us</h1>
-         <h2>Jasmine</h2>
-         <ul class='aboutul'>
-            <li>My favourite hobby is running</li>
-            <li>I do this because I enjoy the mental and physical challenge</li>
-         </ul>
+         <div className="aboutContainer">
+            <h2>Jasmine</h2>
+            <ul className='aboutul'>
+                <li>My favourite hobby is running</li>
+                <li>I do this because I enjoy the mental and physical challenge</li>
+            </ul>
+            <h2>Kharina</h2>
+            <ul className='aboutul'>
+                <li>My favourite hobby is reading</li>
+                <li>I do this because I love exploring new worlds and learning new things</li>
+            </ul>
+         </div>
         </>     
     )
 }
