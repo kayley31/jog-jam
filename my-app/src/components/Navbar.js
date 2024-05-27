e6fb0ca801cca778fd8ed0311aaba53bafa9715f
unknown <roshnigurung08@gmail.com>
2024-05-27 22:22:48 +0100

    Navbar updated so Link can be used to route the pages together and missing alt text added for logo

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -3,21 +3,31 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './Navbar.css';
 import logogrey from '../images/logogrey.png'
+import { Link } from 'react-router-dom';
+
 
 function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" className="navbar">
       <Container>
-        <Navbar.Brand href="#home"><img className="logo" src={logogrey} /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Brand as={Link} to="/"><img className="logo" src={logogrey} alt= "jogjam logo"/></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-          </Nav>
+            <Nav className="me-auto">
+            </Nav>
           <Nav>
-            <Nav.Link className="navlink" href="#find">Find Playlists</Nav.Link>
-            <Nav.Link className="navlink" href="#create">Create Playlists</Nav.Link>
-            <Nav.Link className="navlink" href="#my">My Playlists</Nav.Link>
-            <Nav.Link className="navlink" href="#about">About</Nav.Link>
+            <Nav.Item className="nav-item">
+              <Nav.Link as={Link} to="/find">Find Playlists</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="nav-item">
+              <Nav.Link as={Link} to="/create">Create Playlists</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="nav-item">
+              <Nav.Link as={Link} to="/my">My Playlists</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="nav-item">
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
